4c7f17e5e948a577e64cbdd2a98b701ab6a92824
Debashish kar <debashishkar001@gmail.com>
2026-04-16 09:38:33 +0600

    commit done

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  build: {
+    chunkSizeWarningLimit: 1000,   // 500 → 1000 kB kore dilam (or 1200/1500)
+  }
 })
